9bfac4514fd0d0d548a62c28f3daa6833857d5c7
Abdullah Alakeel <abdul.alakeel@gmail.com>
2022-10-16 04:53:31 +0300

    added explicit types

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,15 +1,15 @@
-import supertest from 'supertest';
+import supertest, { SuperTest } from 'supertest';
 import app from '../index';
 import fs from 'fs';
 import { resizeImage } from '../utilities/imageProcessing';
 import path from 'path';
 
-const request = supertest(app);
+const request: supertest.SuperTest<supertest.Test> = supertest(app);
 
 const imageFolderPath: string = path.resolve(__dirname, '../../assets/images');
 const thumbFolderPath: string = path.resolve(__dirname, '../../assets/thumb');
 
-beforeAll(() => {
+beforeAll((): void => {
   if (fs.existsSync(thumbFolderPath)) {
     // remove thumb if exists
     fs.rmSync(thumbFolderPath, { recursive: true, force: true });
@@ -18,7 +18,7 @@ beforeAll(() => {
   fs.mkdirSync(thumbFolderPath);
 });
 
-afterAll(() => {
+afterAll((): void => {
   // remove thumb if exists
   if (fs.existsSync(thumbFolderPath)) {
     fs.rmSync(thumbFolderPath, { recursive: true, force: true });
@@ -48,7 +48,7 @@ describe('Testing Endpoints Response', (): void => {
     expect(fs.existsSync(path.join(thumbFolderPath, 'fjord-thumb-500-500.jpg'))).toEqual(true);
   });
 
-  it('Get /api/images?filename=unknown endpoint - return error for non existing image filename ', async () => {
+  it('Get /api/images?filename=unknown endpoint - return error for non existing image filename ', async (): Promise<void> => {
     const response = await request.get('/api/images?filename=unknown&width=500&height=500');
     expect(response.status).toBe(404);
     expect(JSON.parse(response.text).Error).toEqual(
@@ -66,7 +66,6 @@ describe('Testing Transform image function ', (): void => {
       path.join(thumbFolderPath, 'encenadaport-thumb.jpg')
     );
     expect(response).toBeDefined();
-
     expect(fs.existsSync(path.join(thumbFolderPath, 'encenadaport-thumb.jpg'))).toEqual(true);
   });
 });
